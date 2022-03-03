b6f108a23950ab856f583bf8bfd1d25756d16b2b
andrewmvk <andrew.medeiros00@gmail.com>
2022-03-03 18:35:02 -0300

    I'm just compiling now... 7

--- src/main/java/rmd/sequelize/Start.java
+++ src/main/java/rmd/sequelize/Start.java
@@ -16,7 +16,7 @@ public class Start {
             String dataBaseURL = System.getenv("SPRING_DATASOURCE_URL");
             System.out.println("USERNAME: "+System.getenv("JDBC_DATABASE_USERNAME"));
             System.out.println("USERNAME: "+System.getenv("JDBC_DATABASE_PASSWORD"));
-            Connection connection = DriverManager.getConnection("postgres://ibwopmmesblhiy:e2ce7681f7d0bfcc6cb3f857449c8abec3adf1ca812b0121218e21ebe617abb3@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d2f7u98g6oo9q7", System.getenv("JDBC_DATABASE_USERNAME"), System.getenv("JDBC_DATABASE_PASSWORD"));
+            Connection connection = DriverManager.getConnection("postgres://ibwopmmesblhiy:e2ce7681f7d0bfcc6cb3f857449c8abec3adf1ca812b0121218e21ebe617abb3@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d2f7u98g6oo9q7");
             return connection;
         } else {
             Properties prop = readPropertiesFile("application.properties");
